f4d27ad1fd67f7474fecf06af7a6f963f79dff7a
Alex Imbrea <alex@elnino.tech>
2020-03-21 21:55:36 +0100

    Add train/prediction loop

--- main.py
+++ main.py
@@ -1,29 +1,63 @@
-from automlstreams.streams import KafkaStream
+import sys
+from automlstreams.meta import LastBestClassifier
 from skmultiflow.evaluation import EvaluatePrequential
-from automlstreams.meta import MetaClassifier
+from kafka import KafkaConsumer, KafkaProducer
+from io import StringIO
+import pandas as pd
+import numpy as np
+
+from random import random
+
 from skmultiflow.trees import HoeffdingTree
 
-DEFAULT_BROKER = 'broker:29092'
-MAX_SAMPLES = 20000
+DEFAULT_BROKER = 'localhost:9092'
 
+def run_indefinetly(input_topic, output_topic, target_index, broker=DEFAULT_BROKER, model=HoeffdingTree()):
+    print(f'Running AutoML for input_topic={input_topic}, output_topic={output_topic} and broker={broker}')
+    consumer = KafkaConsumer(
+            input_topic,
+            bootstrap_servers=broker,
+            group_id=None,
+            auto_offset_reset='earliest',
+            value_deserializer=lambda x: x.decode('utf-8')
+        )
+    producer = KafkaProducer(value_serializer=lambda x: x.encode('utf-8'))
 
-def run(topic, model=MetaClassifier(), broker=DEFAULT_BROKER):
-    print(f'Running demo for topic={topic} and broker={broker}')
-    stream = KafkaStream(topic, bootstrap_servers=broker)
+    total_predictions = 0
+    correct_predictions = 0
+    accuracy = 0
 
-    stream.prepare_for_use()
+    for message in consumer:
+        sample = pd.read_csv(StringIO(message.value), header=None)
 
-    model_name = model.__class__.__name__
+        if any(sample.dtypes == 'object'):
+            print(f'Streamed sample contains text or malformatted data.')
+            continue
+    
+        X = sample.iloc[:,:target_index]
+        y = sample.iloc[:,target_index]
 
-    evaluator = EvaluatePrequential(show_plot=False,
-                                    n_wait=200,
-                                    batch_size=200,
-                                    pretrain_size=500,
-                                    max_samples=MAX_SAMPLES,
-                                    output_file=f'automl-streams/results/meta.{model_name}.{topic}.csv')
-
-    evaluator.evaluate(stream=stream, model=model)
+        if y.isnull().any():
+            # Predict
+            try:
+                y_pred = pd.DataFrame(model.predict(X))
+                producer.send(output_topic, y_pred.to_csv(header=False, index=False))
+            except Exception as e:
+                print('An exception occured during prediction', e)
+        else:
+            # Train
+            try:
+                model.partial_fit(X, y)
+            except Exception as e:
+                print('An exception occured during training', e)
 
 
 if __name__ == "__main__":
-    run('test_topic')
+    try:
+        input_topic = sys.argv[1]
+        output_topic = sys.argv[2]
+        target_index = int(sys.argv[3])
+    except IndexError:
+        raise SystemExit(f"Usage: {sys.argv[0]} input_topic output_topic target_index")
+
+    run_indefinetly(input_topic, output_topic, target_index)
