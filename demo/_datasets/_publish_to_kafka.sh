06684c74efc2e33f85e287addbb0666ceac8b0ca
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2020-04-16 19:12:48 +0200

    Add healtcheck for kafka

--- demo/_datasets/_publish_to_kafka.sh
+++ demo/_datasets/_publish_to_kafka.sh
@@ -3,6 +3,10 @@ if [[ -z "$KAFKA_BROKER" ]]; then
     echo "KAFKA_BROKER is unset or set to the empty string. Using localhost"
 fi
 
+until kafkacat -b ${KAFKA_BROKER:-localhost} -L; do
+  sleep 5
+done
+
 for file in *.csv; do
     echo "Publishing to ${KAFKA_BROKER:-localhost}"
     cat $file | kafkacat -P -b ${KAFKA_BROKER:-localhost} -t ${file%.*}
